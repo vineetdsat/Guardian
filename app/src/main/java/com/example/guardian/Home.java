90bd895be9488e576a3cb0997c637064d8ea68d7
Vineet Singh <vineetdsat@gmail.com>
2020-03-22 23:43:02 +0530

    Remember Me Function and Logout Home Button

--- app/src/main/java/com/example/guardian/Home.java
+++ app/src/main/java/com/example/guardian/Home.java
@@ -2,18 +2,30 @@ package com.example.guardian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 public class Home extends AppCompatActivity {
+    Button Logout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Logout.findViewById(R.id.btn_logout);
+
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/guardian/MainActivity.java
+++ app/src/main/java/com/example/guardian/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.guardian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText Email,Password;
     Button Login;
+    CheckBox Remember;
     TextView goto_reg;
 
     @Override
@@ -22,6 +27,41 @@ public class MainActivity extends AppCompatActivity {
         Password = findViewById(R.id.l_et_pass);
         Login = findViewById(R.id.l_bt_login);
         goto_reg = findViewById(R.id.l_tv);
+        Remember = findViewById(R.id.checkbox2);
+        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+        String checkbox = preferences.getString("remember","");
+
+        if (checkbox.equals("true")){
+            Intent intent = new Intent(MainActivity.this, Home.class);
+            startActivity(intent);
+        }
+        else if (checkbox.equals("false")){
+            Toast.makeText(MainActivity.this,"Please Sign In", Toast.LENGTH_SHORT).show();
+        }
+
+        Remember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (buttonView.isChecked()){
+                    SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember","true");
+                    editor.apply();
+                    Toast.makeText(MainActivity.this,"Checked",Toast.LENGTH_SHORT).show();
+
+                }
+                else if (!buttonView.isChecked()){
+                    SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember","false");
+                    editor.apply();
+                    Toast.makeText(MainActivity.this,"Unchecked",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 
         goto_reg.setOnClickListener(new View.OnClickListener() {
             @Override
