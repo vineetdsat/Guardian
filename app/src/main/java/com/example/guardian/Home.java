cb9d02c04c2b0b019c3b89935b827be2f5bba2ac
Vineet Singh <vineetdsat@gmail.com>
2020-03-23 00:05:56 +0530

    Logout Button

--- app/src/main/java/com/example/guardian/Home.java
+++ app/src/main/java/com/example/guardian/Home.java
@@ -3,6 +3,7 @@ package com.example.guardian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,8 +23,12 @@ public class Home extends AppCompatActivity {
         Logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Home.this, MainActivity.class);
-                startActivity(intent);
+                SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("remember","false");
+                editor.apply();
+
+                finish();
             }
         });
 
