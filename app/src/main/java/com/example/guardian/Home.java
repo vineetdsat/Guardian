3a6b397cabe527fed5b5136810f0efa91710ce7a
Vineet Singh <vineetdsat@gmail.com>
2020-04-05 20:56:30 +0530

    login page

--- app/src/main/java/com/example/guardian/Home.java
+++ app/src/main/java/com/example/guardian/Home.java
@@ -1,17 +1,15 @@
 package com.example.guardian;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
+import android.widget.ImageView;
 import com.google.firebase.auth.FirebaseAuth;
 
 
 public class Home extends AppCompatActivity {
-    Button Logout;
+    ImageView Maps, Web;
 
 
     @Override
@@ -19,15 +17,30 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Logout.findViewById(R.id.btn_logout);
+        Maps = findViewById(R.id.imageView2);
+        Web = findViewById(R.id.imageView4);
 
-        Logout.setOnClickListener(new View.OnClickListener() {
+
+
+        Web.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(Home.this, MainActivity.class);
+                Intent intent = new Intent(Home.this, Web.class);
+                startActivity(intent);
             }
         });
 
+
+
+
+
     }
+
+    public void Logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        finish();
+    }
+
 }
+
