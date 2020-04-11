3462cea4afeecb5d9a4bd97c76562e4e3d7d5722
Vineet Singh <vineetdsat@gmail.com>
2020-04-11 17:24:28 +0530

    user database

--- app/src/main/java/com/example/guardian/Register.java
+++ app/src/main/java/com/example/guardian/Register.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,16 +16,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+    static final String TAG = "YOUR-TAG-NAME";
     EditText Name,Email,Password,Phone,DOB,em_Name,em_Email,em_Phone;
     Button Register;
     TextView goto_login;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
 
 
@@ -44,7 +55,9 @@ public class Register extends AppCompatActivity {
         goto_login = findViewById(R.id.r_tv);
 
 
+
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressb_register);
 
         if(fAuth.getCurrentUser() != null){
@@ -56,8 +69,15 @@ public class Register extends AppCompatActivity {
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = Email.getText().toString().trim();
+                final String email = Email.getText().toString().trim();
                 String pass = Password.getText().toString().trim();
+                final String name = Name.getText().toString().trim();
+                final String phone = Phone.getText().toString().trim();
+                final String dob = DOB.getText().toString().trim();
+                final String em_name = em_Name.getText().toString().trim();
+                final String em_email = em_Email.getText().toString().trim();
+                final String em_phone = em_Phone.getText().toString().trim();
+
 
                 if (TextUtils.isEmpty(email)){
                     Email.setError("Email is Required");
@@ -75,6 +95,22 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
                             Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("Name",name);
+                            user.put("Email",email);
+                            user.put("Phone",phone);
+                            user.put("D.O.B",dob);
+                            user.put("Em_Name",em_name);
+                            user.put("Em_Email",em_email);
+                            user.put("Em_Phone",em_phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG,"OnSuccess: User Profile is Created for "+ userID);
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         }
                         else {
