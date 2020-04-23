ca549988bae05103db135bf233268d2e10c96bbf
Vineet Singh <vineetdsat@gmail.com>
2020-04-23 23:13:11 +0530

    remember me

--- app/src/main/java/com/example/guardian/Home.java
+++ app/src/main/java/com/example/guardian/Home.java
@@ -1,8 +1,10 @@
 package com.example.guardian;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
     ImageView Maps, Web,SOS;
+    Button Logout;
 
 
     @Override
@@ -22,6 +25,7 @@ public class Home extends AppCompatActivity {
         Maps = findViewById(R.id.home_iv_map);
         Web = findViewById(R.id.home_iv_web);
         SOS = findViewById(R.id.home_iv_sos);
+        Logout = findViewById(R.id.bt_home_lout);
 
 
 
@@ -46,16 +50,23 @@ public class Home extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("remember","false");
+                editor.apply();
+                finish();
+            }
+        });
 
 
 
-    }
-
-    public void Logout(View view) {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-        finish();
     }
 
 }
+
+
 
--- app/src/main/java/com/example/guardian/MainActivity.java
+++ app/src/main/java/com/example/guardian/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.guardian;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     TextView goto_reg;
     ProgressBar progress;
     FirebaseAuth fAuth;
+    CheckBox Remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,17 @@ public class MainActivity extends AppCompatActivity {
         Login = findViewById(R.id.l_bt_login);
         goto_reg = findViewById(R.id.l_tv);
         progress = findViewById(R.id.progressb_login);
+        Remember = findViewById(R.id.checkbox_remember);
+
+        SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
+        String checkbox = preferences.getString("remember","");
+        if (checkbox.equals("true")){
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        else if (checkbox.equals("false")){
+            Toast.makeText(MainActivity.this,"Please Sign In", Toast.LENGTH_SHORT).show();
+        }
 
 
 
@@ -68,6 +83,27 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+            }
+        });
+        Remember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (buttonView.isChecked()){
+                    SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember","true");
+                    editor.apply();
+                    Toast.makeText(MainActivity.this,"Checked",Toast.LENGTH_SHORT).show();
+
+                }
+                else if (!buttonView.isChecked()){
+                    SharedPreferences preferences = getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("remember","false");
+                    editor.apply();
+                    Toast.makeText(MainActivity.this,"Unchecked",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
