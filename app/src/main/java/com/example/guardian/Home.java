aefff734e95136f627e51fe8feea569668d0ed7d
Vineet Singh <vineetdsat@gmail.com>
2020-04-28 03:36:11 +0530

    home

--- app/src/main/java/com/example/guardian/Home.java
+++ app/src/main/java/com/example/guardian/Home.java
@@ -5,48 +5,51 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 
 public class Home extends AppCompatActivity {
-    ImageView Maps, Web,SOS;
     Button Logout;
+    CardView Map,Sos,News,Area,Call,Profile;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Map = findViewById(R.id.cardView_map);
+        Sos = findViewById(R.id.cardView_sos);
+        News = findViewById(R.id.cardView_news);
+        Area = findViewById(R.id.cardView_area);
+        Call = findViewById(R.id.cardView_call);
+        Profile = findViewById(R.id.cardView_profile);
+        Logout = findViewById(R.id.bt_logout);
 
-        Maps = findViewById(R.id.home_iv_map);
-        Web = findViewById(R.id.home_iv_web);
-        SOS = findViewById(R.id.home_iv_sos);
-        Logout = findViewById(R.id.bt_home_lout);
 
 
 
-        Web.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Home.this, Web.class);
-                startActivity(intent);
-            }
-        });
-        SOS.setOnClickListener(new View.OnClickListener() {
+        Sos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Home.this, SOS.class);
                 startActivity(intent);
             }
         });
-        Maps.setOnClickListener(new View.OnClickListener() {
+        Map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Home.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+        Call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, emergencyCall.class);
                 startActivity(intent);
             }
         });
